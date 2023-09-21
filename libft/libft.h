93275da30431baae93036f58036f1ab228cd7f5a
Abby_en_redes <abbyenredes@gmail.com>
2023-09-21 21:54:20 +0200

    new file docs

--- libft/libft.h
+++ libft/libft.h
@@ -6,15 +6,13 @@
 /*   By: abigamas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:59:51 by abigamas          #+#    #+#             */
-/*   Updated: 2023/09/20 14:00:25 by abigamas         ###   ########.fr       */
+/*   Updated: 2023/09/21 21:52:49 by abigamas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <unistd.h>
-# include <stdio.h>
-# include <ctype.h>
 
  int	ft_isalpha(int c);
  int	ft_isdigit(int c);
