f6efb0e5d4f098bca858f9a199446edf0961293d
Abby_en_redes <abbyenredes@gmail.com>
2023-10-04 18:07:22 +0200

    various functions

---
+++ libft/ft_bzero.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abigamas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 18:06:12 by abigamas          #+#    #+#             */
+/*   Updated: 2023/10/04 18:06:17 by abigamas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//function writes n zeroed bytes to the string s.
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = (unsigned char) 0;
+		++i;
+	}
+}
