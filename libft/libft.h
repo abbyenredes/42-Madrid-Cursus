e8b58add856f532d1e93698d7e63032ac731b217
Abby_en_redes <abbyenredes@gmail.com>
2023-10-04 17:58:36 +0200

    add more funtions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abigamas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:59:51 by abigamas          #+#    #+#             */
-/*   Updated: 2023/09/29 15:59:02 by abigamas         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:56:59 by abigamas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,25 @@
 # define LIBFT_H
 # include <unistd.h>
 
- int	ft_isalpha(int c);
- int	ft_isdigit(int c);
- int	ft_isalnum(int c);
- int	ft_isascii(int c);
- int	ft_isprint(int c);
- int	ft_toupper(int c);
- int	ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(char *str);
 #endif
