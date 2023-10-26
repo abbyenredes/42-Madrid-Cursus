ab4c5ddbe4848f034ce771ed730751041e344859
Abby_en_redes <abbyenredes@gmail.com>
2023-10-26 20:11:47 +0200

    agregue la segunda parte

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abigamas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:59:51 by abigamas          #+#    #+#             */
-/*   Updated: 2023/10/04 18:44:09 by abigamas         ###   ########.fr       */
+/*   Updated: 2023/10/26 20:10:52 by abigamas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,17 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(char *str);
+char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t nitems, size_t size);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strtrim(char const *s1, char const *set);
 #endif
